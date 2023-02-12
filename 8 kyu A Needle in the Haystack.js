77df34d91cf02d0e0f659e3201c3ca7edb88a5ef
Lemeshkov <evlem3250@gmail.com>
2023-02-12 10:54:52 +0300

    8kyu

---
+++ 8 kyu A Needle in the Haystack.js
@@ -0,0 +1,6 @@
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle");
+}
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOff("needle");
+}
